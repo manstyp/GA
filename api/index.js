5165012f302200264ab95aced9aa8390064b0339
manstyp <115995992+manstyp@users.noreply.github.com>
2024-01-19 07:46:36 +0000

    startar om codespace

--- api/index.js
+++ api/index.js
@@ -1,15 +1,31 @@
 // ALWAYS RUN WHILE BUILDING --> npx tailwindcss -i ./style/style.css -o ./public/output.css --watch
-
+const mongoose = require("mongoose");
+const morgan = require("morgan");
 const express = require("express");
 const app = express();
 const port = 3002;
 const path = require("path");
 const viewsDirectory = path.join(process.cwd(), "views");
 
+app.use(morgan("dev"));
 app.use(express.static("public"));
-
 app.set("views", viewsDirectory);
 app.set("view engine", "pug");
+
+const database = "scores";
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster.5snl1t7.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.connect(uri);
+mongoose.connection
+  .on("open", () => {
+    console.log("Mongoose is connected :D");
+  })
+  .on("closed", () => {
+    console.log("Mongoose is NOT connected :(");
+  })
+  .on("error", (error) => {
+    console.log("Error", error);
+  });
 
 app.listen(port, () => {
   console.log("Server started on port " + port);
