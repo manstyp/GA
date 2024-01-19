9b612580a4b8951abe849d2b716a053cbf7223c9
manstyp <115995992+manstyp@users.noreply.github.com>
2024-01-19 08:27:54 +0000

    säkerhet och form för register sida

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const express = require("express");
 const app = express();
-const port = 3002;
+const port = 3003;
 const path = require("path");
 const viewsDirectory = path.join(process.cwd(), "views");
 
@@ -12,20 +12,16 @@ app.use(express.static("public"));
 app.set("views", viewsDirectory);
 app.set("view engine", "pug");
 
-const database = "scores";
 const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster.5snl1t7.mongodb.net/?retryWrites=true&w=majority`;
 
-mongoose.connect(uri);
-mongoose.connection
-  .on("open", () => {
+mongoose.connect(uri).then(
+  (client) => {
     console.log("Mongoose is connected :D");
-  })
-  .on("closed", () => {
-    console.log("Mongoose is NOT connected :(");
-  })
-  .on("error", (error) => {
-    console.log("Error", error);
-  });
+  },
+  (reason) => {
+    console.log(`Failed due to ${reason}`);
+  }
+);
 
 app.listen(port, () => {
   console.log("Server started on port " + port);
@@ -46,4 +42,8 @@ app.get("/login", (req, res) => {
 
 app.get("/register", (req, res) => {
   res.render("register");
+});
+
+app.get("*", (req, res) => {
+  res.send("Route error");
 });
