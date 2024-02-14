9f614f20904c024a449db3b2d942c20fc8da5d11
manstyp <115995992+manstyp@users.noreply.github.com>
2024-02-14 11:04:24 +0000

    ändrar storlek på username uppe höger

---
+++ public/js/script.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const passwordInput = document.querySelector("#password");
+  const eye = document.querySelector("#eye");
+
+  eye.addEventListener("click", function () {
+    if (passwordInput.type === "password") {
+      passwordInput.type = "text";
+      eye.classList.remove("fa-eye");
+      eye.classList.add("fa-eye-slash");
+    } else {
+      passwordInput.type = "password";
+      eye.classList.remove("fa-eye-slash");
+      eye.classList.add("fa-eye");
+    }
+  });
+});
+
+window.onload = function () {
+  const usernameElement = document.querySelector("#username");
+  const username = usernameElement.getAttribute("href").split("/").pop();
+
+  console.log(username);
+  if (username.length <= 6) {
+    usernameElement.classList.remove("w-32");
+    usernameElement.classList.add("w-24");
+  }
+  if (username.length > 8 > 12) {
+    usernameElement.classList.remove("w-32");
+    usernameElement.classList.add("w-36");
+  }
+  if (username.length > 12) {
+    usernameElement.classList.remove("w-32");
+    usernameElement.classList.add("w-48");
+  }
+};
